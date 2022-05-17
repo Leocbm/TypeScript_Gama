9581967b9bf6677b13d22282eb8337b3abbed1fe
Leonardo Cunha <leonardoleo.barbosa2@gmail.com>
2022-05-17 12:20:50 -0300

    add arquivos e configurações

---
+++ src/1-tipos-basicos/string.ts
@@ -0,0 +1,16 @@
+
+let nome: string;
+
+
+// ...
+
+
+nome = 'leonardo';
+
+const nomeComValor = 'leonardo'
+
+function nomeCompleto(nome: string, sobrenome: string) {
+    return `${nome} ${sobrenome}`
+}
+
+nomeCompleto('leonardo', 'cunha')
